002d1545ba4bd35214111f2c471f5af247ea6d51
Jasper Shovelton <jrshovelton@gmail.com>
2023-03-25 19:57:27 +0000

    add custom header file

---
+++ blocks.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+		/*Icon*/   /*Command*/       /*Update Interval*/  /*Update Signal*/
+		{" ",      "dwmbl_mic.sh",   0,                   1},
+		{"",       "dwmbl_vol.sh",   0,                   2},
+		{"",       "dwmbl_cpubar",   5,                   3},
+		{"",       "dwmbl_mem.sh",   10,                  4},
+		{"",       "dwmbl_bat.sh",   30,                  5},
+		{"",       "dwmbl_dat.sh",   10,                  6},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;

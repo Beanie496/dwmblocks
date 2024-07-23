5b2d881ba4603bec8cdf8b27c96ca66b06b32c01
Jasper Shovelton <Beanie.github@proton.me>
2024-07-23 19:33:27 +0100

    Update gap.

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-		/*Icon*/   /*Command*/       /*Update Interval*/  /*Update Signal*/
-		{"[",      "dwmbl_mic.sh",   0,                   1},
-		{"",       "dwmbl_vol.sh",   0,                   2},
-		{"",       "dwmbl_cpubar",   5,                   3},
-		{"",       "dwmbl_tmp.sh",   5,                   4},
-		{"",       "dwmbl_mem.sh",   10,                  5},
-		{"",       "dwmbl_bat.sh",   30,                  6},
-		{"",       "dwmbl_dat.sh",   10,                  7},
+		/*Icon*/   /*Command*/        /*Update Interval*/  /*Update Signal*/
+		{"[",      "dwmbl_mic.sh",    0,                   1},
+		{"",       "dwmbl_vol.sh",    0,                   2},
+		{"",       "dwmbl_cpubar",    5,                   3},
+		{"",       "dwmbl_tmp.sh",    5,                   4},
+		{"",       "dwmbl_mem.sh",    10,                  5},
+		{"",       "dwmbl_bat.sh",    30,                  6},
+		{"",       "dwmbl_dat.sh",    10,                  7},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
